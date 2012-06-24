115f7f83fe79f21ec4876f56435bd7372fda4190
Steve Rowley <srowley@gmail.com>
2012-06-23 18:11:21 -0700

    pid aggregate spec added; just copied original commented-out tests

---
+++ spec/models/aggregates/pid_aggregate_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+# TODO: these tests were commented out in the original suite. Investigate.
+
+describe ActiveWarehouse::Aggregate::PidAggregate, :new => true do
+  #   def test_populate
+  #    assert_nothing_raised do
+  #      agg = ActiveWarehouse::Aggregate::PidAggregate.new(MultiDimensionalRegionalSalesCube)
+  #      agg.populate
+  #    end
+  #   end
+  # 
+  #   def test_query
+  #    agg = ActiveWarehouse::Aggregate::PidAggregate.new(MultiDimensionalRegionalSalesCube)
+  #    agg.populate
+  #    results = agg.query(:date, :cy, :store, :region)
+  #   end
+end
