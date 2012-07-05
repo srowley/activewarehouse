a68716a66dc7f5729f7aa0179b94e865d1ca3972
Steve Rowley <srowley@gmail.com>
2012-07-04 17:18:59 -0700

    missed one last spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ RSpec.configure do |config|
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[("./support/**/*.rb")].each {|f| require f}
-    
+      
   config.before(:suite) do
     set_up_classes
     FactoryGirl.reload
