de9934a8dc81b7093be121aeaa51ea26df868cb2
Steve Rowley <srowley@gmail.com>
2012-07-04 20:01:17 -0700

    uses ENV['DB'] to establish ActiveRecord connection

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,18 @@ require 'activewarehouse'
 require 'factory_girl_rails'
 require 'support/setup'
 
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+# Basically stolen from will_paginate's ActiveRecord test setup.
+db = ENV['DB'].blank?? 'sqlite3' : ENV['DB']
+  
+configurations = YAML.load_file(File.expand_path('../config/database.yml', __FILE__))
+raise "no configuration for '#{db}'" unless configurations.key? db
+
+configuration = configurations[db]  
+ActiveRecord::Base.configurations = { db => configuration }
+ActiveRecord::Base.establish_connection(db)
+
+# TODO: figure out why setting tz to UTC breaks one of the SCD specs.
+# ActiveRecord::Base.default_timezone = :utc
 
 RSpec.configure do |config|
   # Requires supporting ruby files with custom matchers and macros, etc,
@@ -13,7 +24,7 @@ RSpec.configure do |config|
       
   config.before(:suite) do
     set_up_classes
-    FactoryGirl.reload
+    FactoryGirl.reload # Don't think FactoryGirl can register factories for classes until classes exist
   end
   
 end
