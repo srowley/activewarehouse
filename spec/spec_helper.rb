0c60500d2107291c20a022deec8470868ad80eda
Steve Rowley <srowley@gmail.com>
2012-07-04 21:58:14 -0700

    see if random tz changes help

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,7 +29,7 @@ end
 ActiveRecord::Base.establish_connection(db)
 
 # TODO: figure out why setting tz to UTC breaks one of the SCD specs.
-# ActiveRecord::Base.default_timezone = :utc
+ActiveRecord::Base.default_timezone = :utc
 
 RSpec.configure do |config|
   # Requires supporting ruby files with custom matchers and macros, etc,
