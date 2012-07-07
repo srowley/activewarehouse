639195f98da9c95723b7f7073c2e1640381d7fa1
Steve Rowley <srowley@gmail.com>
2012-07-07 04:47:19 -0700

    forgot to uncomment offending cube specs.

--- spec/models/cube_spec.rb
+++ spec/models/cube_spec.rb
@@ -4,11 +4,9 @@ describe ActiveWarehouse::Cube do
   
   before(:all) do
     
-    # TODO: figure out what it is about creating PosSalesTransaction records that 
-    # breaks the no_aggregate test even if the table is cleaned out after.
-    # @sale = FactoryGirl.create(:january_2001_florida_sale)
-    # @sale = FactoryGirl.create(:january_2002_florida_sale)
-    # @sale = FactoryGirl.create(:january_2002_florida_sale)
+    @sale = FactoryGirl.create(:january_2001_florida_sale)
+    @sale = FactoryGirl.create(:january_2002_florida_sale)
+    @sale = FactoryGirl.create(:january_2002_florida_sale)
 
     create_class("RegionalSalesCube", ActiveWarehouse::Cube)
     RegionalSalesCube.reports_on :pos_retail_sales_transaction
@@ -124,177 +122,173 @@ describe ActiveWarehouse::Cube do
       expect { RegionalSalesCube.populate }.to_not raise_error
     end
   end
-  
-
 
   describe "#query" do
-#     Depends on the records which break the no_aggregate test so commenting out
-#     since I know they pass, until I can redo these tests rationally and DRY them.
     
-#     before(:all) do
-#       result = RegionalSalesCube.query(
-#         :column_dimension_name => :date, 
-#         :column_hierarchy_name => :cy, 
-#         :row_dimension_name => :store, 
-#         :row_hierarchy_name => :region
-#         )
-#     
-#       @values_2001 = result.values('Southeast', '2001')
-#       @values_2002 = result.values('Southeast', '2002')
-#       @values_2003 = result.values('Southeast', '2003')
-#     end
-#     
-#     it "returns a value for each aggregate field" do
-#       @values_2001.should have(6).items
-#       @values_2002.should have(6).items
-#     end
-#     
-#     it "returns correct results for a cube cell with one record" do
-#       @values_2001['Sum of Sales Quantity'].should == 1
-#       @values_2001['Sum of Sales Amount'].should be_within(0.01).of(1.75)
-#       @values_2001['Sum of Cost'].should be_within(0.01).of(0.5)
-#       @values_2001['Sum of Gross Profit'].should be_within(0.01).of(1.25)
-#       @values_2001['Sales Quantity Count'].should == 1
-#       @values_2001['Avg Sales Amount'].should be_within(0.01).of(1.75)
-#     end
-#     
-#     it "returns correct results for a cube cell with multiple records" do
-#       @values_2002['Sum of Sales Quantity'].should == 4
-#       @values_2002['Sum of Sales Amount'].should be_within(0.01).of(5.50)
-#       @values_2002['Sum of Cost'].should be_within(0.01).of(5)
-#       @values_2002['Sum of Gross Profit'].should be_within(0.01).of(0.50)
-#       @values_2002['Sales Quantity Count'].should == 2
-#       @values_2002['Avg Sales Amount'].should be_within(0.01).of(2.75)
-#     end
-# 
-#     it "returns hash of zeroes for values in cell for which there are no facts" do
-#       @values_2003.should == {'Sum of Sales Quantity' => 0,
-#                                'Sum of Sales Amount' => 0,
-#                                'Sum of Cost' => 0,
-#                                'Sum of Gross Profit' => 0,
-#                                'Sales Quantity Count' => 0,
-#                                'Avg Sales Amount' => 0}
-#     end
-#   
-#   # The test upon which the following examples are based seems to more or less 
-#   # repeat the tests for the NoAggregate class, but I suppose in theory here you are
-#   # checking that the query method works when passed a cube, and there you are checking
-#   # that it works when passed an aggregate. Seems like you could just make sure the
-#   # cube version is passing on the message to the aggregate class and call it a day
-#   # for testing the Cube method.
-#   
-#   it "returns correct results when passed parameters as a simple array" do
-#     cube = RegionalSalesCube.new
-#     #   assert_query_success(cube) NOTE: not replacing this, because it effectively re-ran the test above
-#     this_result = cube.query(:date, :cy, :store, :region)
-#     @values_2001 = this_result.values('Southeast', '2001')
-#     @values_2002 = this_result.values('Southeast', '2002')
-#     @values_2003 = this_result.values('Southeast', '2003')
-#     
-#     @values_2001.should have(6).items
-#     @values_2002.should have(6).items
-#     
-#     @values_2001['Sum of Sales Quantity'].should == 1
-#     @values_2001['Sum of Sales Amount'].should be_within(0.01).of(1.75)
-#     @values_2001['Sum of Cost'].should be_within(0.01).of(0.5)
-#     @values_2001['Sum of Gross Profit'].should be_within(0.01).of(1.25)
-#     @values_2001['Sales Quantity Count'].should == 1
-#     @values_2001['Avg Sales Amount'].should be_within(0.01).of(1.75)    
-#     @values_2002['Sum of Sales Quantity'].should == 4
-#     @values_2002['Sum of Sales Amount'].should be_within(0.01).of(5.50)
-#     @values_2002['Sum of Cost'].should be_within(0.01).of(5)
-#     @values_2002['Sum of Gross Profit'].should be_within(0.01).of(0.50)
-#     @values_2002['Sales Quantity Count'].should == 2
-#     @values_2002['Avg Sales Amount'].should be_within(0.01).of(2.75)
-#     @values_2003.should == {'Sum of Sales Quantity' => 0,
-#                              'Sum of Sales Amount' => 0,
-#                              'Sum of Cost' => 0,
-#                              'Sum of Gross Profit' => 0,
-#                              'Sales Quantity Count' => 0,
-#                              'Avg Sales Amount' => 0}
-#   end
-#   
-#   it "returns correct results for drilldown queries" do
-#     cube = RegionalSalesCube.new
-#     
-#     filters = {
-#       'date.calendar_year' => '2001',
-#       'date.calendar_quarter' => 'Q1',
-#       'date.calendar_month_name' => 'January',
-#       'date.calendar_week' => 'Week 1'
-#     }
-#     
-#     this_result = cube.query(
-#       :column_dimension_name => :date, 
-#       :column_hierarchy_name => :cy, 
-#       :row_dimension_name => :store, 
-#       :row_hierarchy_name => :region, 
-#       :conditions => nil, 
-#       :cstage => 4, 
-#       :rstage => 0, 
-#       :filters => filters
-#     )
-#     
-#     @values_monday = this_result.values('Southeast', 'Monday')
-#     @values_tuesday = this_result.values('Southeast', 'Tuesday')
-#     
-#     @values_monday['Sum of Sales Quantity'].should == 1
-#     @values_monday['Sum of Sales Amount'].should be_within(0.01).of(1.75)
-#     @values_monday['Sum of Cost'].should be_within(0.01).of(0.5)
-#     @values_monday['Sum of Gross Profit'].should be_within(0.01).of(1.25)
-#     @values_monday['Sales Quantity Count'].should == 1
-#     @values_monday['Avg Sales Amount'].should be_within(0.01).of(1.75)    
-# 
-#     @values_tuesday.should == {'Sum of Sales Quantity' => 0,
-#                              'Sum of Sales Amount' => 0,
-#                              'Sum of Cost' => 0,
-#                              'Sum of Gross Profit' => 0,
-#                              'Sales Quantity Count' => 0,
-#                              'Avg Sales Amount' => 0}
-#     
-#     filters = {'date.calendar_year' => '2001', 'store.store_region' => 'Southeast'}
-#     
-#     this_result = cube.query(
-#       :column_dimension_name => :date, 
-#       :column_hierarchy_name => :cy, 
-#       :row_dimension_name => :store, 
-#       :row_hierarchy_name => :region, 
-#       :conditions => nil, 
-#       :cstage => 1, 
-#       :rstage => 1, 
-#       :filters => filters
-#     )
-#     
-#     @values_Q1 = this_result.values('South Florida', 'Q1')
-#     @values_Q1['Sum of Sales Quantity'].should == 1
-#     @values_Q1['Sum of Sales Amount'].should be_within(0.01).of(1.75)
-#     @values_Q1['Sum of Cost'].should be_within(0.01).of(0.5)
-#     @values_Q1['Sum of Gross Profit'].should be_within(0.01).of(1.25)
-#     @values_Q1['Sales Quantity Count'].should == 1
-#     @values_Q1['Avg Sales Amount'].should be_within(0.01).of(1.75)
-#     
-#   end
-#   
-#   it "returns correct results for queries with the same row/column names" do
-#     cube = RegionalSalesCube.new
-#   
-#     this_result = cube.query(
-#         # ORIGINAL NOTE: I'd prefer to use different dimensions here, but
-#         # can't find two that have the same column names.  This is a 
-#         # simple way to get two dimensions with the same column names.
-#         :column_dimension_name => :date, 
-#         :column_hierarchy_name => :cy, 
-#         :row_dimension_name => :date, 
-#         :row_hierarchy_name => :cy
-#       )
-#       
-#     # TODO: man, this ugly. If he were dead, David Chelimsky
-#     # would be turning over in his grave.
-#     values = this_result.values("2001", "2001").values
-#     values.any? { |v| v > 0 }.should be_true
-#     values = this_result.values("2002", "2002").values
-#     values.any? { |v| v > 0 }.should be_true
-#   end
+    before(:all) do
+      result = RegionalSalesCube.query(
+        :column_dimension_name => :date, 
+        :column_hierarchy_name => :cy, 
+        :row_dimension_name => :store, 
+        :row_hierarchy_name => :region
+        )
+    
+      @values_2001 = result.values('Southeast', '2001')
+      @values_2002 = result.values('Southeast', '2002')
+      @values_2003 = result.values('Southeast', '2003')
+    end
+    
+    it "returns a value for each aggregate field" do
+      @values_2001.should have(6).items
+      @values_2002.should have(6).items
+    end
+    
+    it "returns correct results for a cube cell with one record" do
+      @values_2001['Sum of Sales Quantity'].should == 1
+      @values_2001['Sum of Sales Amount'].should be_within(0.01).of(1.75)
+      @values_2001['Sum of Cost'].should be_within(0.01).of(0.5)
+      @values_2001['Sum of Gross Profit'].should be_within(0.01).of(1.25)
+      @values_2001['Sales Quantity Count'].should == 1
+      @values_2001['Avg Sales Amount'].should be_within(0.01).of(1.75)
+    end
+    
+    it "returns correct results for a cube cell with multiple records" do
+      @values_2002['Sum of Sales Quantity'].should == 4
+      @values_2002['Sum of Sales Amount'].should be_within(0.01).of(5.50)
+      @values_2002['Sum of Cost'].should be_within(0.01).of(5)
+      @values_2002['Sum of Gross Profit'].should be_within(0.01).of(0.50)
+      @values_2002['Sales Quantity Count'].should == 2
+      @values_2002['Avg Sales Amount'].should be_within(0.01).of(2.75)
+    end
+
+    it "returns hash of zeroes for values in cell for which there are no facts" do
+      @values_2003.should == {'Sum of Sales Quantity' => 0,
+                               'Sum of Sales Amount' => 0,
+                               'Sum of Cost' => 0,
+                               'Sum of Gross Profit' => 0,
+                               'Sales Quantity Count' => 0,
+                               'Avg Sales Amount' => 0}
+    end
+  
+  # The test upon which the following examples are based seems to more or less 
+  # repeat the tests for the NoAggregate class, but I suppose in theory here you are
+  # checking that the query method works when passed a cube, and there you are checking
+  # that it works when passed an aggregate. Seems like you could just make sure the
+  # cube version is passing on the message to the aggregate class and call it a day
+  # for testing the Cube method.
+  
+  it "returns correct results when passed parameters as a simple array" do
+    cube = RegionalSalesCube.new
+    #   assert_query_success(cube) NOTE: not replacing this, because it effectively re-ran the test above
+    this_result = cube.query(:date, :cy, :store, :region)
+    @values_2001 = this_result.values('Southeast', '2001')
+    @values_2002 = this_result.values('Southeast', '2002')
+    @values_2003 = this_result.values('Southeast', '2003')
+    
+    @values_2001.should have(6).items
+    @values_2002.should have(6).items
+    
+    @values_2001['Sum of Sales Quantity'].should == 1
+    @values_2001['Sum of Sales Amount'].should be_within(0.01).of(1.75)
+    @values_2001['Sum of Cost'].should be_within(0.01).of(0.5)
+    @values_2001['Sum of Gross Profit'].should be_within(0.01).of(1.25)
+    @values_2001['Sales Quantity Count'].should == 1
+    @values_2001['Avg Sales Amount'].should be_within(0.01).of(1.75)    
+    @values_2002['Sum of Sales Quantity'].should == 4
+    @values_2002['Sum of Sales Amount'].should be_within(0.01).of(5.50)
+    @values_2002['Sum of Cost'].should be_within(0.01).of(5)
+    @values_2002['Sum of Gross Profit'].should be_within(0.01).of(0.50)
+    @values_2002['Sales Quantity Count'].should == 2
+    @values_2002['Avg Sales Amount'].should be_within(0.01).of(2.75)
+    @values_2003.should == {'Sum of Sales Quantity' => 0,
+                             'Sum of Sales Amount' => 0,
+                             'Sum of Cost' => 0,
+                             'Sum of Gross Profit' => 0,
+                             'Sales Quantity Count' => 0,
+                             'Avg Sales Amount' => 0}
+  end
+  
+  it "returns correct results for drilldown queries" do
+    cube = RegionalSalesCube.new
+    
+    filters = {
+      'date.calendar_year' => '2001',
+      'date.calendar_quarter' => 'Q1',
+      'date.calendar_month_name' => 'January',
+      'date.calendar_week' => 'Week 1'
+    }
+    
+    this_result = cube.query(
+      :column_dimension_name => :date, 
+      :column_hierarchy_name => :cy, 
+      :row_dimension_name => :store, 
+      :row_hierarchy_name => :region, 
+      :conditions => nil, 
+      :cstage => 4, 
+      :rstage => 0, 
+      :filters => filters
+    )
+    
+    @values_monday = this_result.values('Southeast', 'Monday')
+    @values_tuesday = this_result.values('Southeast', 'Tuesday')
+    
+    @values_monday['Sum of Sales Quantity'].should == 1
+    @values_monday['Sum of Sales Amount'].should be_within(0.01).of(1.75)
+    @values_monday['Sum of Cost'].should be_within(0.01).of(0.5)
+    @values_monday['Sum of Gross Profit'].should be_within(0.01).of(1.25)
+    @values_monday['Sales Quantity Count'].should == 1
+    @values_monday['Avg Sales Amount'].should be_within(0.01).of(1.75)    
+
+    @values_tuesday.should == {'Sum of Sales Quantity' => 0,
+                             'Sum of Sales Amount' => 0,
+                             'Sum of Cost' => 0,
+                             'Sum of Gross Profit' => 0,
+                             'Sales Quantity Count' => 0,
+                             'Avg Sales Amount' => 0}
+    
+    filters = {'date.calendar_year' => '2001', 'store.store_region' => 'Southeast'}
+    
+    this_result = cube.query(
+      :column_dimension_name => :date, 
+      :column_hierarchy_name => :cy, 
+      :row_dimension_name => :store, 
+      :row_hierarchy_name => :region, 
+      :conditions => nil, 
+      :cstage => 1, 
+      :rstage => 1, 
+      :filters => filters
+    )
+    
+    @values_Q1 = this_result.values('South Florida', 'Q1')
+    @values_Q1['Sum of Sales Quantity'].should == 1
+    @values_Q1['Sum of Sales Amount'].should be_within(0.01).of(1.75)
+    @values_Q1['Sum of Cost'].should be_within(0.01).of(0.5)
+    @values_Q1['Sum of Gross Profit'].should be_within(0.01).of(1.25)
+    @values_Q1['Sales Quantity Count'].should == 1
+    @values_Q1['Avg Sales Amount'].should be_within(0.01).of(1.75)
+    
+  end
+  
+  it "returns correct results for queries with the same row/column names" do
+    cube = RegionalSalesCube.new
+  
+    this_result = cube.query(
+        # ORIGINAL NOTE: I'd prefer to use different dimensions here, but
+        # can't find two that have the same column names.  This is a 
+        # simple way to get two dimensions with the same column names.
+        :column_dimension_name => :date, 
+        :column_hierarchy_name => :cy, 
+        :row_dimension_name => :date, 
+        :row_hierarchy_name => :cy
+      )
+      
+    # TODO: man, this ugly. If he were dead, David Chelimsky
+    # would be turning over in his grave.
+    values = this_result.values("2001", "2001").values
+    values.any? { |v| v > 0 }.should be_true
+    values = this_result.values("2002", "2002").values
+    values.any? { |v| v > 0 }.should be_true
+  end
   
   end
    
